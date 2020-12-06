d4ad6f9c5cbbe27d7feb84f1a4041eaa7f0091ee
Benoit Bayszczak <b.bayszczak@gmail.com>
2020-12-06 21:29:24 +0100

    add examples

--- pkg/drivers/switchprocontroller/SwitchProController_test.go
+++ pkg/drivers/switchprocontroller/SwitchProController_test.go
@@ -43,3 +43,53 @@ func Example_blocking() {
 	// A:0
 	// x:0.000000 - y:0.000000
 }
+
+func ExampleNewSwitchProController() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+}
+
+func ExampleSwitchProController_GetStick() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+	leftStick, _ := controller.GetStick("left")
+	fmt.Printf("x:%f - y:%f\n", leftStick.X, leftStick.Y)
+	// Output: x:0.000000 - y:0.000000
+}
+
+func ExampleSwitchProController_StartListener() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+}
+
+func ExampleSwitchProController_GetButton_released() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+	aButton, _ := controller.GetButton("a")
+	fmt.Printf("name:%s - state:%d\n", aButton.Name, aButton.State)
+	// Output: name:a - state:0
+}
+
+func ExampleSwitchProController_GetButton_pressed() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+	aButton, _ := controller.GetButton("a")
+	fmt.Printf("name:%s - state:%d\n", aButton.Name, aButton.State)
+	// Output: name:a - state:1
+}
+
+func ExampleSwitchProController_GetButtonState_released() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+	aState, _ := controller.GetButtonState("a")
+	fmt.Printf("state:%d\n", aState)
+	// Output: state:0
+}
+
+func ExampleSwitchProController_GetButtonState_pressed() {
+	controller := switchprocontroller.NewSwitchProController()
+	controller.StartListener(0)
+	aState, _ := controller.GetButtonState("a")
+	fmt.Printf("state:%d\n", aState)
+	// Output: state:1
+}
